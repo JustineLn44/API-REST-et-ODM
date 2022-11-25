8c3883964b2cc3ef2ba2c39576154491443ed3f0
JustineLn44 <lenautout.justine@gmail.com>
2022-11-25 14:19:07 +0100

    modify response to json / add editStock function

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,47 +4,40 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'app_products', methods: ['GET'])]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, ProductRepository $productRepo): JsonResponse
     {
-        $products = $doctrine->getRepository(Product::class)->findAll();
-
-        return $this->render('product/index.html.twig', [
-            'products' => $products,
-        ]);
+        return $this->json($productRepo->findAll());
     }
 
-    #[Route('/product/add', name: 'app_product_new',  methods: ['GET', 'POST'])]
-    public function addProduct(Request $request, ManagerRegistry $doctrine): Response
+    #[Route('/product/add', name: 'app_product_new',  methods: ['POST'])]
+    public function addProduct(Request $request, ManagerRegistry $doctrine, ProductRepository $productRepo): Response
     {
         $entityManager = $doctrine->getManager();
         $product = new Product();
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
+        $product->setName($request->get('name'));
+        $product->setStock($request->get('stock'));
 
-        if ($form->isSubmitted() && $form->isValid()){
-            $entityManager->persist($product);
-            $entityManager->flush();
+        $entityManager->persist($product);
+        $entityManager->flush();
 
-            return $this->redirectToRoute('app_products', [], Response::HTTP_SEE_OTHER);
-        }
-        return $this->render('product/add.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
+        return $this->json($productRepo->find($product->getId()));
     }
 
     #[Route('/product/show/{id}', name: 'app_product_show',  methods: ['GET'])]
-    public function showProduct(ManagerRegistry $doctrine, int $id): Response
+    public function showProduct(ManagerRegistry $doctrine, int $id, ProductRepository $productRepo): Response
     {
+        $entityManager = $doctrine->getManager();
         $product = $doctrine->getRepository(Product::class)->find($id);
 
         if (!$product) {
@@ -53,29 +46,28 @@ class ProductController extends AbstractController
             );
         }
 
-        return $this->render('product/show.html.twig', [
-            'product' => $product,
-        ]);
+        return $this->json($productRepo->find($id));
     }
 
     #[Route('/product/edit/{id}', name: 'edit_product', methods: ['GET', 'POST'])]
-    public function editProduct(Request $request, ManagerRegistry $doctrine, int $id): Response
+    public function editProduct(Request $request, ManagerRegistry $doctrine, int $id, ProductRepository $productRepo): Response
     {
         $entityManager = $doctrine->getManager();
         $product = $doctrine->getRepository(Product::class)->find($id);
-        $form = $this->createForm(ProductType::class, $product);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_products', [], Response::HTTP_SEE_OTHER);
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
         }
-        return $this->render('product/add.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
+
+        $product->setName($request->get('name'));
+        $product->setStock($request->get('stock'));
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json($productRepo->find($product->getId()));
     }
 
     #[Route('/product/delete/{id}', name: 'app_product_delete', methods: ['GET', 'POST'])]
@@ -89,11 +81,29 @@ class ProductController extends AbstractController
                 'No product found for id '.$id
             );
         }
-
         $entityManager->remove($product);
         $entityManager->flush();
 
-        return $this->redirectToRoute('app_products', [], Response::HTTP_SEE_OTHER);
+        return $this->json('Product '.$id.' is delete');
+    }
 
+    #[Route('/product/editStock/{id}', name: 'app_editStock_product', methods: ['GET', 'POST'])]
+    public function editStockProduct(Request $request, ManagerRegistry $doctrine, int $id, ProductRepository $productRepo): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $product = $doctrine->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $product->setStock($product->getStock() + $request->get('modificationStock'));
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->json($productRepo->find($product->getId()));
     }
 }
