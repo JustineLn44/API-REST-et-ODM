73fa7cb3341f4cefee99c8a3616087bdc9c6b465
JustineLn44 <lenautout.justine@gmail.com>
2022-11-25 15:00:31 +0100

    add error message json

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -21,12 +22,24 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/add', name: 'app_product_new',  methods: ['POST'])]
-    public function addProduct(Request $request, ManagerRegistry $doctrine, ProductRepository $productRepo): Response
+    public function addProduct(Request $request, ManagerRegistry $doctrine, ProductRepository $productRepo, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
         $product = new Product();
+
+        if (!$request->get('name') || !$request->get('stock')){
+            return $this->json('Please enter the correct fields (name, stock)');
+        }
+        if (!is_numeric($request->get('stock'))){
+            return $this->json('Please entre a number for the stock');
+        }
+        
         $product->setName($request->get('name'));
         $product->setStock($request->get('stock'));
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
 
         $entityManager->persist($product);
         $entityManager->flush();
@@ -41,28 +54,36 @@ class ProductController extends AbstractController
         $product = $doctrine->getRepository(Product::class)->find($id);
 
         if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            return $this->json('No product found for id '.$id);
         }
 
         return $this->json($productRepo->find($id));
     }
 
-    #[Route('/product/edit/{id}', name: 'edit_product', methods: ['GET', 'POST'])]
-    public function editProduct(Request $request, ManagerRegistry $doctrine, int $id, ProductRepository $productRepo): Response
+    #[Route('/product/edit/{id}', name: 'edit_product', methods: ['PUT'])]
+    public function editProduct(Request $request, ManagerRegistry $doctrine, int $id, ProductRepository $productRepo, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
         $product = $doctrine->getRepository(Product::class)->find($id);
-
+        
         if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            return $this->json('No product found for id '.$id);
+        }
+        
+        if (!$request->get('name') || !$request->get('stock')){
+            return $this->json('Please enter the correct fields (name, stock)');
+        }
+        if (!is_numeric($request->get('stock'))){
+            return $this->json('Please entre a number for the stock');
         }
 
         $product->setName($request->get('name'));
         $product->setStock($request->get('stock'));
+
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
 
         $entityManager->persist($product);
         $entityManager->flush();
@@ -70,33 +91,33 @@ class ProductController extends AbstractController
         return $this->json($productRepo->find($product->getId()));
     }
 
-    #[Route('/product/delete/{id}', name: 'app_product_delete', methods: ['GET', 'POST'])]
-    public function deleteProduct(ManagerRegistry $doctrine, int $id): Response
+    #[Route('/product/delete/{id}', name: 'app_product_delete', methods: ['POST'])]
+    public function deleteProduct(ManagerRegistry $doctrine, int $id, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
         $product = $entityManager->getRepository(Product::class)->find($id);
 
         if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            return $this->json('No product found for id '.$id);
         }
+
         $entityManager->remove($product);
         $entityManager->flush();
 
         return $this->json('Product '.$id.' is delete');
     }
 
-    #[Route('/product/editStock/{id}', name: 'app_editStock_product', methods: ['GET', 'POST'])]
+    #[Route('/product/editStock/{id}', name: 'app_editStock_product', methods: ['PUT'])]
     public function editStockProduct(Request $request, ManagerRegistry $doctrine, int $id, ProductRepository $productRepo): Response
     {
         $entityManager = $doctrine->getManager();
         $product = $doctrine->getRepository(Product::class)->find($id);
 
         if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+            return $this->json('No product found for id '.$id);
+        }
+        if (!is_numeric($request->get('stock'))){
+            return $this->json('Please entre a number for the stock');
         }
 
         $product->setStock($product->getStock() + $request->get('modificationStock'));
